8e215c63b2baab2ec978ab6abb2848786990c52a
nicolasmilville <nicolas.milville@lrqdo.fr>
2024-03-01 16:27:12 +0100

    SF5 upgrade

--- Command/RunCommand.php
+++ Command/RunCommand.php
@@ -445,6 +445,6 @@ class RunCommand extends Command
 
     private function getEntityManager(): EntityManager
     {
-        return /** @var EntityManager */ $this->registry->getManagerForClass('JMSJobQueueBundle:Job');
+        return /** @var EntityManager */ $this->registry->getManagerForClass(Job::class);
     }
 }
