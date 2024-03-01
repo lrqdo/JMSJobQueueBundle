c948519faa41d8b32e960d04de42c0c92fb6bc0d
nicolasmilville <nicolas.milville@lrqdo.fr>
2024-03-01 16:51:42 +0100

    SF5 upgrade

--- Entity/Listener/PersistentRelatedEntitiesCollection.php
+++ Entity/Listener/PersistentRelatedEntitiesCollection.php
@@ -318,7 +318,7 @@ class PersistentRelatedEntitiesCollection implements Collection, Selectable
      * @param mixed $key
      * @param mixed $value
      */
-    public function set($key, $value)
+    public function set($key, $value): void
     {
         throw new \LogicException('set() is not supported.');
     }
@@ -445,7 +445,7 @@ class PersistentRelatedEntitiesCollection implements Collection, Selectable
     /**
      * Clears the collection.
      */
-    public function clear()
+    public function clear(): void
     {
         throw new \LogicException('clear() is not supported.');
     }
